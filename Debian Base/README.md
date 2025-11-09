b15fb9648241da00babc69d0a3ae128b49089e5d
Matt Stocker <matthewstocker@gmail.com>
2025-11-09 00:33:02 +0000

    Added script for Debian KDE installation

---
+++ Debian Base/README.md
@@ -0,0 +1,62 @@
+# Debian Base Script
+
+Setting up Debian (running KDE) to play games and other apps.
+
+## What does this script do?
+
+It does the following:
+
+- Downloads and installs Discord
+- Downloads and installs Google Chrome
+- Downloads and installs Dopamine (Music Player)
+- Downloads and configures NVIDIA driver
+- Downloads and installs Steam
+- Installs default JRE (Java Runtime Environment)
+- Installs PokeMMO client (via Flatpak)
+- Installs NotepadQQ
+
+The script creates a folder called `Installers` in your home directory for it to store the latest installers.  Once the script has finished, this folder is then removed.
+
+## How to run
+
+1. Load up a terminal window
+2. Run the following command in a Terminal to download and run the config script\
+   `curl https://raw.githubusercontent.com/matthewstocker/System-Configuration-Scripts/main/Irons%20Zorin%2018/zorin-gaming-config.sh | bash`
+3. Follow the script through to completion.
+4. You will get a window pop up about Steam, click on **Start Steam** button.
+   ![Press start steam](https://github.com/matthewstocker/System-Configuration-Scripts/blob/main/Irons%20Zorin%2018/img/steam1.png)
+5. You will then get a terminal pop up.  
+   ![](https://github.com/matthewstocker/System-Configuration-Scripts/blob/main/Irons%20Zorin%2018/img/steam2.png)  
+   Press Enter to allow the updates to take place (you may get asked to enter your password here).
+7. It will then ask you to install some packages.
+   ![](https://github.com/matthewstocker/System-Configuration-Scripts/blob/main/Irons%20Zorin%2018/img/steam3.png)  
+   Press enter to install the needed packages.
+8. Enjoy!
+
+## Issues?
+
+Please create an issue in GitHub and I'll take a look!
+
+## License
+
+MIT License
+
+Copyright (c) 2025 Matt Stocker
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ Debian Base/debian_base.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+# Script to set up a Debian 13 KDE installation
+
+# Script setup
+# Make temp folder for installation files
+printf 'Creating temporary folders for installers'
+echo '======================================================================='
+mkdir ~/Installers
+echo '======================================================================='
+
+# NVIDIA Drivers
+echo 'NVIDIA Driver Prep'
+echo '======================================================================='
+echo 'Enabling Non-Free Repos'
+echo '======================================================================='
+sudo sed -i 's/main/main non-free contrib/g' /etc/apt/sources.list
+
+# Install Flatpak
+echo 'Installing Flatpak support'
+echo '======================================================================='
+sudo apt install flatpak
+sudo apt install plasma-discover-backend-flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+echo '======================================================================='
+
+# Add Google Chrome repo
+echo 'Adding Google Chrome repo'
+echo '======================================================================='
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+echo '======================================================================='
+
+# Grab installation files needed
+echo 'Downloading Discord .deb package'
+echo '======================================================================='
+wget -O ~/Installers/discord.deb 'https://discord.com/api/download?platform=linux&format=deb'
+echo '======================================================================='
+
+echo 'Downloading latest Dopamine release from GitHub'
+echo '======================================================================='
+dopamine_url=$(curl -s https://api.github.com/repos/digimezzo/dopamine/releases/latest | jq -r '(.assets[].browser_download_url | select(. | contains(".deb")))')
+wget -O ~/Installers/dopamine.deb $dopamine_url
+echo '======================================================================='
+
+echo 'Downloading latest Dopamine release from GitHub'
+echo '======================================================================='
+obsidian_url=$(curl -s https://api.github.com/repos/obsidianmd/obsidian-releases/releases/latest | jq -r '(.assets[].browser_download_url | select(. | contains("_amd64.deb")))')
+wget -O ~/Installers/obsidian.deb $obsidian_url
+echo '======================================================================='
+
+echo 'Downloading latest Steam installer'
+echo '======================================================================='
+wget -O ~/Installers/steam.deb 'https://cdn.akamai.steamstatic.com/client/installer/steam.deb'
+echo '======================================================================='
+
+# Update repos
+echo 'Updating repos'
+echo '======================================================================='
+sudo apt update -y
+echo '======================================================================='
+
+# Install apps
+
+# Install Flatpaks
+echo 'Installing PokeMMO (Flatpak)'
+echo '======================================================================='
+flatpak install -y com.pokemmo.PokeMMO
+echo '======================================================================='
+
+# Install from repo
+echo 'Installing Java Runtime Environment'
+echo '======================================================================='
+sudo apt install -y default-jre
+echo '======================================================================='
+
+echo 'Installing NotepadQQ'
+echo '======================================================================='
+sudo apt install notepadqq
+echo '======================================================================='
+
+echo 'Installing Google Chrome'
+echo '======================================================================='
+sudo apt-get install google-chrome-stable
+echo '======================================================================='
+
+# Install .deb files
+echo 'Installing Discord'
+echo '======================================================================='
+sudo dpkg -i ~/Installers/discord.deb
+echo '======================================================================='
+
+echo 'Installing Steam'
+echo '======================================================================='
+sudo dpkg -i ~/Installers/steam.deb 
+echo '======================================================================='
+
+echo 'Installing Dopamine'
+echo '======================================================================='
+sudo dpkg -i ~/Installers/dopamine.deb
+echo '======================================================================='
+
+echo 'Installing Obsidian'
+echo '======================================================================='
+sudo dpkg -i ~/Installers/dopamine.deb
+echo '======================================================================='
+
+# Configure and install NVIDIA driver
+echo 'Installing NVIDIA driver and building kernel module'
+echo '======================================================================='
+sudo apt install nvidia-driver nvidia-kernel-dkms
+echo '======================================================================='
+
+# Finally remove the temp folder
+echo 'Clearing up installers'
+echo '======================================================================='
+rm -rf ~/Installers
+
+echo 'Script complete! Enjoy!'
+echo 'You NEED to reboot the computer to finish installation!'
